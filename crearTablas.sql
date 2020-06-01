a69a4f639b84fe62f52a97618e5be6a2c7a35fa2
Leandro Favio Gomez Racca <leo1ragomez95@gmail.com>
2020-06-01 19:02:10 +0200

    basic tables

--- crearTablas.sql
+++ crearTablas.sql
@@ -6,3 +6,54 @@ CREATE TABLE joguines (codi_barres int PRIMARY KEY,
 		preu int,
 		tipus varchar(255) FOREIGN KEY (tipus),
 		complement_de FOREIGN KEY)
+
+CREATE TABLE equivalent (
+) ENGINE=InnoDB;
+
+CREATE TABLE clients (
+		codi int UNSIGNED AUTO_INCREMENT not null PRIMARY KEY, --No se si hace falta el not null
+		nom varchar(255),
+		email varchar(255)
+)  ENGINE=InnoDB;
+
+CREATE TABLE encarrec (
+		client int,
+		joguina int,
+		data date,
+		qt int,
+		CONSTRAINT pk_encarrec PRIMARY KEY (client, joguina),
+		CONSTRAINT fk_client_encarrec FOREIGN KEY (client) REFERENCES clients(codi),
+		CONSTRAINT fk_joguina_encarrec FOREIGN KEY (joguina) REFERENCES joguines(codi_barres)
+) ENGINE=InnoDB;
+
+CREATE TABLE factura (
+		client int,
+		data date,
+		joguina int,
+		qt int,
+		preu_un double,
+		CONSTRAINT pk_factura PRIMARY KEY (client, data, joguina),
+		CONSTRAINT fk_client_factura FOREIGN KEY (client) REFERENCES clients(codi),
+		CONSTRAINT fk_joguina_factura FOREIGN KEY (joguina) REFERENCES joguines(codi_barres)
+) ENGINE=InnoDB;
+
+CREATE TABLE particulars (
+) ENGINE=InnoDB;
+
+CREATE TABLE empreses (
+) ENGINE=InnoDB;
+
+CREATE TABLE fills (
+) ENGINE=InnoDB;
+
+CREATE TABLE persones_contacte (
+) ENGINE=InnoDB;
+
+CREATE TABLE parentesc (
+		particular int,
+		parent int,
+		grau int,
+		CONSTRAINT pk_parentesc PRIMARY KEY (particular, parent),
+		CONSTRAINT fk_particular_parentesc FOREIGN KEY (particular) REFERENCES particular(codi),
+		CONSTRAINT fk_parent_parentesc FOREIGN KEY (particular) REFERENCES particular(codi)
+)ENGINE=InnoDB;
