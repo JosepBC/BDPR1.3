08aff332ad758b4daf9d3c81a85f7588f519275b
Josep Bello Curto <belloamposta@gmail.com>
2020-06-05 01:36:58 +0200

    Consulta A ya ordena, seguramente se puede optimizar

--- consultaA.sql
+++ consultaA.sql
@@ -1,7 +1,18 @@
-SELECT *
-FROM Particulars
-WHERE codi = 
-	(SELECT codi
-	FROM Fills
-	WHERE
-		(SELECT DATEDIFF(CURRENT_DATE, data_naix)) > 6570);
+SELECT
+	*
+FROM 
+	Clients
+WHERE codi IN 
+	(SELECT
+	 	codi 
+	 FROM 
+	 	Particulars
+	 WHERE codi IN
+	 	(SELECT
+	 		codi
+	 	FROM
+	 		Fills
+	 	WHERE
+	 		(SELECT DATEDIFF(CURRENT_DATE, data_naix) > 6570)))
+ORDER BY
+	nom;
