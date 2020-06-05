c3a3a2b5329a5e0080175f18ea698f519cc5a666
Josep Bello Curto <belloamposta@gmail.com>
2020-06-05 21:40:11 +0200

    FK ON DELETE CASCADE

--- crearTablas.sql
+++ crearTablas.sql
@@ -56,7 +56,7 @@ CREATE TABLE Encarrec (
         data date,
         qt int,
         CONSTRAINT pk_encarrec PRIMARY KEY (client, joguina),
-        CONSTRAINT fk_client_encarrec FOREIGN KEY (client) REFERENCES Clients(codi),
+        CONSTRAINT fk_client_encarrec FOREIGN KEY (client) REFERENCES Clients(codi) ON DELETE CASCADE,
         CONSTRAINT fk_joguina_encarrec FOREIGN KEY (joguina) REFERENCES Joguines(codi_barres),
         CONSTRAINT ck_encarrec CHECK (10 < qt AND qt < 1000)
 ) ENGINE = InnoDB;
@@ -68,7 +68,7 @@ CREATE TABLE Factura (
         qt int,
         preu_un double,
         CONSTRAINT pk_factura PRIMARY KEY (client, data, joguina),
-        CONSTRAINT fk_client_factura FOREIGN KEY (client) REFERENCES Clients(codi),
+        CONSTRAINT fk_client_factura FOREIGN KEY (client) REFERENCES Clients(codi) ON DELETE CASCADE,
         CONSTRAINT fk_joguina_factura FOREIGN KEY (joguina) REFERENCES Joguines(codi_barres)
 ) ENGINE = InnoDB;
 
@@ -76,13 +76,13 @@ CREATE TABLE Particulars (
         codi int PRIMARY KEY,
         telf int,
         dni varchar(255),
-        CONSTRAINT fk_codi_particulars FOREIGN KEY (codi) REFERENCES Clients(codi)
+        CONSTRAINT fk_codi_particulars FOREIGN KEY (codi) REFERENCES Clients(codi) ON DELETE CASCADE
 ) ENGINE = InnoDB;
 
 CREATE TABLE Empreses (
         codi int PRIMARY KEY,
         nif varchar(255) NOT NULL UNIQUE,
-        CONSTRAINT fk_codi_empreses FOREIGN KEY (codi) REFERENCES Clients(codi)
+        CONSTRAINT fk_codi_empreses FOREIGN KEY (codi) REFERENCES Clients(codi) ON DELETE CASCADE
 ) ENGINE = InnoDB;
 
 DELIMITER $$
@@ -112,7 +112,7 @@ CREATE PROCEDURE InsertClient(IN nom varchar(255), IN email varchar(255), IN tlf
             VALUES (@last_id,
                 tlf,
                 dni);
-            
+
             INSERT INTO Empreses (codi,
                 nif)
             VALUES (@last_id,
@@ -135,7 +135,7 @@ CREATE TABLE Persones_contacte (
         codi int,
         telef int,
         CONSTRAINT pk_persones_contacte PRIMARY KEY (nom, codi),
-        CONSTRAINT fk_codi_persones_contacte FOREIGN KEY (codi) REFERENCES Empreses(codi)
+        CONSTRAINT fk_codi_persones_contacte FOREIGN KEY (codi) REFERENCES Empreses(codi) ON DELETE CASCADE
 ) ENGINE = InnoDB;
 
 CREATE TABLE Parentesc (
