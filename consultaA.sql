889fc25f02b6292ba20616c61b3d93eabbae278f
Josep Bello Curto <belloamposta@gmail.com>
2020-06-06 15:57:00 +0200

    Consulta A optimizada

--- consultaA.sql
+++ consultaA.sql
@@ -1,18 +1,11 @@
 SELECT
-	*
-FROM 
-	Clients
-WHERE codi IN 
-	(SELECT
-	 	codi 
-	 FROM 
-	 	Particulars
-	 WHERE codi IN
-	 	(SELECT
-	 		codi
-	 	FROM
-	 		Fills
-	 	WHERE
-	 		(SELECT DATEDIFF(CURRENT_DATE, data_naix) > 6570)))
-ORDER BY
-	nom;
+	Particulars.codi AS Pare_Id,
+	Clients.nom AS Nom_Pare,
+	Clients.email AS Email_pare,
+	Particulars.telf AS Pare_Tlf,
+	Particulars.dni AS Pare_DNI
+FROM ((Clients
+INNER JOIN Particulars ON Clients.codi = Particulars.codi)
+INNER JOIN Fills ON Fills.codi = Particulars.codi)
+WHERE DATEDIFF(CURRENT_DATE, data_naix) > 6570 
+ORDER BY Clients.nom;
